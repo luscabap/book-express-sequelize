a51bb7aa9a1705e73e5bf4ad8030d34f1c8accb4
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-08 13:40:54 -0300

    Feat: associating migrations

--- src/migrations/20240608162734-create-matricula.js
+++ src/migrations/20240608162734-create-matricula.js
@@ -12,6 +12,16 @@ module.exports = {
       status: {
         type: Sequelize.STRING,
       },
+      estudante_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "pessoas", key: "id" }
+      },
+      curso_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { models: "cursos", key: "id" }
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
